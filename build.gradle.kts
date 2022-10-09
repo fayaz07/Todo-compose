9de17313f1da37aaff975e5df1b5c002408e9704
Mohammad Fayaz <fayazfz07@gmail.com>
2022-10-09 13:01:27 +0530

    add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,35 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id(Plugins.androidApplication) version PluginVersions.androidApplication apply false
     id(Plugins.androidLibrary) version PluginVersions.androidApplication apply false
     id(Plugins.kotlinAndroid) version PluginVersions.kotlinAndroid apply false
+    id(Plugins.detekt) version PluginVersions.detekt apply true
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(BuildPlugins.detekt)
+    }
+}
+
+detekt {
+    toolVersion = PluginVersions.detekt
+    config = files("$rootDir/detekt.yml")
+}
+
+tasks.withType<Detekt> {
+    include("**/*.kt")
+    include("**/*.kts")
+    exclude(".*/resources/.*")
+    exclude(".*/build/.*")
+    exclude("/versions.gradle.kts")
+    exclude("buildSrc/settings.gradle.kts")
 }
 
 tasks.register<Delete>("clean").configure {
--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -2,9 +2,16 @@ object Plugins {
     const val androidApplication = "com.android.application"
     const val androidLibrary = "com.android.library"
     const val kotlinAndroid = "org.jetbrains.kotlin.android"
+    const val detekt = "io.gitlab.arturbosch.detekt"
 }
 
 object PluginVersions {
     const val androidApplication = "7.3.0"
     const val kotlinAndroid = "1.7.20"
+
+    const val detekt = "1.22.0-RC1"
+}
+
+object BuildPlugins {
+    const val detekt = "io.gitlab.arturbosch.detekt:detekt-formatting:${PluginVersions.detekt}"
 }
