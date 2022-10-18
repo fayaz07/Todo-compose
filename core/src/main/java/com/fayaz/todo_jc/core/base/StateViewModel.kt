74198ec7f8176be4da2ce7f4657893ed13986f2f
Mohammad Fayaz <fayaz.mohammad@thoughtworks.com>
2022-10-18 20:27:15 +0530

    replace state with stateflow

--- core/src/main/java/com/fayaz/todo_jc/core/base/StateViewModel.kt
+++ core/src/main/java/com/fayaz/todo_jc/core/base/StateViewModel.kt
@@ -1,16 +1,15 @@
 package com.fayaz.todo_jc.core.base
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 abstract class StateViewModel<UiEvent: ViewEvent, UiState: ViewState>: ViewModel() {
   private val initialState: UiState by lazy { setInitialState() }
   abstract fun setInitialState(): UiState
 
-  private val _uiState: MutableState<UiState> = mutableStateOf(initialState)
-  val viewState: State<UiState> = _uiState
+  private val _uiState = MutableStateFlow(initialState)
+  val viewState = _uiState.asStateFlow()
 
   abstract fun dispatcher(event: UiEvent)
 
